11bfb40290fc61e87e9b84c0ccef442109a3071e
rushi1717 <rushiharke1717@gmail.com>
2023-07-21 12:21:08 +0530

    first commit

---
+++ src/main/java/com/met/learndevops/LearnDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.met.learndevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/met/learndevops/LearnDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.met.learndevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
